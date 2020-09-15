297329bc3d1ed337c6ea2b77ece239e7b529eeca
Axmin <ahwn@pm.me>
2020-09-15 22:41:27 +0545

    clean:code

--- server/index.js
+++ server/index.js
@@ -1,12 +1,27 @@
 const express = require("express");
-const app = express();
-const port = process.env.PORT || 8080;
+const fs = require("fs");
 
-const planesList = require("./data/list_of_planes.json");
+const app = express();
+const port = process.env.PORT || 8081;
+
+const planesListRaw = fs.readFileSync("./data/list_of_planes.json");
 
 app.get("/", (req, res) => {
 	res.header("Content-Type", "application/json");
-	res.json(planesList);
+	const data = JSON.parse(planesListRaw);
+	res.json(data.tables);
+});
+
+app.get("/tables_length", (req, res) => {
+	res.header("Content-Type", "application/json");
+	const data = JSON.parse(planesListRaw);
+	res.send(data.tables.length + "");
+});
+
+app.get("/tables", (req, res) => {
+	res.header("Content-Type", "application/json");
+	const data = JSON.parse(planesListRaw);
+	res.json(data.tables);
 });
 
 app.listen(port, () => {
