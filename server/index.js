245df10ac3abc000ffedfc88083a4c25f9fd9002
Axmin <ahwn@pm.me>
2020-09-16 22:43:08 +0545

    Add: planes_list route

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const planesListRaw = fs.readFileSync("./data/list_of_planes.json");
 const planesList = JSON.parse(planesListRaw);
 
 // This middleware will be executed for every request to the app
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
 	res.header("Content-Type", "application/json");
 	next();
 });
@@ -25,26 +25,25 @@ app.get("/tables", (req, res) => {
 	res.json(planesList.tables[0].rows[0]);
 });
 
-const zip = (rows) => rows[0].map((_, c) => rows.map((row) => row[c]));
+// const zip = (rows) => rows[0].map((_, c) => rows.map((row) => row[c]));
 const tableToObj = function (table) {
 	const rows = Object.values(table.rows);
 	const head = rows.shift().columns;
-	const values = rows.map((i) => zip([Object.values(head), Object.values(i.columns)]));
+	// const values = rows.map((i) => zip([Object.values(head), Object.values(i.columns)]));
+	const values = rows.map((i) => {
+		const data = {};
+		Object.values(head).forEach((o, id) => {
+			data[o] = i.columns[id];
+		});
+		return data;
+	});
 	return values;
 };
 
 app.get("/planes_list", (req, res) => {
 	const tables = planesList.tables;
 	const rows = tableToObj(tables[0]);
-	const data = [];
-
-	rows.forEach((i) => {
-		const row = [];
-		row.push(i.map((o) => ({ [o[0]]: o[1] })));
-		data.push(row);
-	});
-
-	res.json(data);
+	res.json(rows);
 });
 
 app.listen(port, () => {
